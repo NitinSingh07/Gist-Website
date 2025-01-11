f64370830e223782b0efe1507898695cfc697eb5
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-11 19:13:04 +0530

    Refactor multer configuration; streamline file upload setup and maintain consistent filename formatting

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -2,13 +2,12 @@ const Job = require("../models/job.model");
 const multer = require("multer");
 const path = require("path");
 
-// Set up multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Specify the directory to save uploaded files
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to the filename
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
