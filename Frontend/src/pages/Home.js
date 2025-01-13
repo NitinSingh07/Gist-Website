18068105e852ffa174107c9850ac1f638f327b9e
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-01-13 17:05:18 +0530

    Comment out JobSearchForm import and component in Home page

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from '../components/Home/HeroSection';
-import JobSearchForm from '../components/Home/JobSearchForm';
+// import JobSearchForm from '../components/Home/JobSearchForm';
 import LatestJobOpenings from '../components/Home/LatestJobOpenings';
 import TalentSection from '../components/Home/TalentSection';
 import Footer from '../components/common/Footer';
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div>
       <HeroSection/>
-      <JobSearchForm/>
+      {/* <JobSearchForm/> */}
       <LatestJobOpenings/>
       <TalentSection />
       <Footer/>
